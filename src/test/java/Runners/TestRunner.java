0fcb15bfd7901c7e7e8ff534b03dfc34ac67a067
sudless <ssudedemir@icloud.com>
2025-08-19 15:51:15 +0300

    Initial project commit

--- src/test/java/Runners/TestRunner.java
+++ src/test/java/Runners/TestRunner.java
@@ -9,7 +9,6 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/main/resources/features",
         glue = {"stepDefinitions", "hooks"},
-//        tags = "@autoComplete",
         plugin = {"pretty",
                   "json:target/cucumber-report/cucumber.json",
                   "junit:target/cucumber-report/cucumber.xml"
